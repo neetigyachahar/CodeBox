015d5289285c7ec1b77fa88ca170430f89b0070e
neetigya chahar <neetigyachahar784@gmail.com>
2021-07-16 14:47:15 +0530

    pereject commit

--- src/containers/Layout.tsx
+++ src/containers/Layout.tsx
@@ -1,6 +1,46 @@
+
+import { useEffect } from 'react'
+import { Box } from '@material-ui/core'
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
+
+
+import Prism from "prismjs"
+
+const useStyles = makeStyles(({ palette, breakpoints }: Theme) =>
+    createStyles({
+        container: {
+            display: 'flex',
+        },
+        a: {
+            flex: 1
+        }
+    })
+)
+
 const Layout = () => {
+    const classes = useStyles();
+
+    useEffect(() => {
+        setTimeout(() => Prism.highlightAll(), 100)
+    }, []);
+
     return (
-        <div>leyout</div>
+        <Box className={classes.container}>
+            <Box className={classes.a}>
+                <pre className="line-numbers">
+                    <code className="language-js">
+                        {`
+                            const foo = 'foo';
+                            const bar = 'bar';
+                            console.log(foo + bar);
+                        `}
+                    </code>
+                </pre>
+            </Box>
+            <Box className={classes.a}>
+                data
+            </Box>
+        </Box>
     )
 }
 
--- src/routes/Codebox.tsx
+++ src/routes/Codebox.tsx
@@ -1,15 +1,7 @@
 import { FC } from 'react'
-import { createStyles, Theme, makeStyles } from '@material-ui/core/styles'
 import Layout from '../containers/Layout'
 
-const useStyles = makeStyles(({ palette, breakpoints }: Theme) =>
-    createStyles({
-
-    })
-)
-
 const Codebox: FC = () => {
-    const classes = useStyles();
     return (
         <Layout />
     )
